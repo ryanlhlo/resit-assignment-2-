2b504af7daedf336a91cb3a839183657daf3cfa6
ryanlhlo <ryanlo20041012@icloud.com>
2026-04-10 17:57:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// jQuery fade-in effect for article columns on page load
-// Source idea: adapted from basic jQuery fade examples on W3Schools
+// script.js
+// This file adds a simple fade-in effect to the article columns.
+// Source idea adapted from basic jQuery fade examples on W3Schools.
+
 $(document).ready(function () {
-    $('.column').each(function (index) {
+    $(".column").each(function (index) {
         $(this)
-            .css('opacity', 0)
+            .css("opacity", 0)
             .delay(180 * index)
             .fadeTo(700, 1);
     });
